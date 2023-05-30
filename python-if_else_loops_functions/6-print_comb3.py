8d93a191ae6d48a70252c6e3da12735999d8c383
solp22 <sol.puentereal@gmail.com>
2023-05-30 09:30:53 -0700

    pycodestyle fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,5 @@
 for digit1 in range(8):
     for digit2 in range(10):
         if digit1 < digit2:
-                print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
 print(89)
-
