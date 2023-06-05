731dace7fed816329eb054a05d129f4b8575c85e
solp22 <sol.puentereal@gmail.com>
2023-06-05 09:50:07 -0700

    task 6 fix?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
-            for val in row:
+            for val in range(len(matrix[row])):
                 if (val < len(matrix[row]) - 1):
                     print("{:d}".format(val), end=" ")
                 else:
