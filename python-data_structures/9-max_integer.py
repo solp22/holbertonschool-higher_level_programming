54b86be5e829886768c37c1e1c05987dfcf50730
solp22 <sol.puentereal@gmail.com>
2023-06-05 11:19:28 -0700

    task 9 fix

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    if my_list:
-        for i in my_list:
-            if i > max:
-                max = i
-        return max
-    return None
+    if not my_list:
+        return None
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
+
