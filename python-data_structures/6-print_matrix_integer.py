f1bd0a86adbcb98535b090d42b880febda4ea559
solp22 <sol.puentereal@gmail.com>
2023-06-05 08:14:49 -0700

    format fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for val in row:
-            print(val, end=" ")
+            print("{}".format(val), end=" ")
         print()
