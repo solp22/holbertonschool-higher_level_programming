37277e917f2f708180cd93df2c69d973114c8894
solp22 <sol.puentereal@gmail.com>
2023-06-05 09:52:47 -0700

    idk

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
-            for val in range(len(matrix[row])):
-                if (val < len(matrix[row]) - 1):
-                    print("{:d}".format(val), end=" ")
-                else:
-                    print("{:d}".format(val), end="")
+            for val in row:
+                print("{:d}".format(val), end=" ")
             print()
+    return
