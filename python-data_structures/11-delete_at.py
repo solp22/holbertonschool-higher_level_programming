e48f891c31aa11ee4af677beb22000b269a93724
solp22 <sol.puentereal@gmail.com>
2023-06-05 11:55:31 -0700

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
-    for i in my_list:
-        if i == idx:
-            my_list.remove(i)
+    del my_list[idx]
     return my_list
