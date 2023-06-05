6c5e02739a303b7c6a6f994942869bb3887f81f1
solp22 <sol.puentereal@gmail.com>
2023-06-05 09:20:13 -0700

    task 6 fix?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for val in row:
-            print("{:d}".format(val), end=" ")
-        print()
+    if matrix:
+        for row in matrix:
+            for val in row:
+                print("{:d}".format(val), end=" ")
+            print()
