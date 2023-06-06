eb77e5ef51f923f6ffcd3108ddd44d76a446449c
solp22 <sol.puentereal@gmail.com>
2023-06-06 10:49:02 -0700

    task 10 fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
     max_value = max(a_dictionary)
     return max_value
