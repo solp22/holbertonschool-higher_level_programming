ed2d5a156a11df18cfd39bffae5c1f05add5adcc
solp22 <sol.puentereal@gmail.com>
2023-06-06 12:33:23 -0700

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
     convert = {'M': 1000, 'D': 500, 'C': 100,
                'L': 50, 'X': 10, 'V': 5, 'I': 1}
     decimal = 0
@@ -8,4 +10,6 @@ def roman_to_int(roman_string):
             if decimal < convert[letter]:
                 decimal *= -1
             decimal += convert[letter]
+        else:
+            return 0
     return (decimal)
