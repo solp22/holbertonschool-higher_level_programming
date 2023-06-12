d67c1987f5b606a248def7dbf7a0653985e82645
solp22 <sol.puentereal@gmail.com>
2023-06-12 09:08:35 -0700

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Square:
+    """instantiation with optional size"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
