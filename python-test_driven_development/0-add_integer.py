6670072f6a4474143cc123f1954610597be7dad4
solp22 <sol.puentereal@gmail.com>
2023-06-14 07:46:14 -0700

    comment error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+'''add_integer module'''
 def add_integer(a, b=98):
-    """add integers"""
+    '''add integers'''
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
