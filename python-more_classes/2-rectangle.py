18ddd6f745969ff94622472d7880a0232bd9da98
root <sol.puentereal@gmail.com>
2023-06-16 08:10:27 -0700

    pycodestyle

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -35,8 +35,8 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
-        return (self.width + self.height) * 2 
+        return (self.width + self.height) * 2
