abf12de4240a30624e7e4cef0ee7befd496f8e1c
solp22 <sol.puentereal@gmail.com>
2023-06-20 07:22:25 -0700

    documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""define is_same_class"""
 def is_same_class(obj, a_class):
+    """eturns True if the object is exactly an instance of
+    the specified class; otherwise False"""
     return isinstance(obj, a_class)
