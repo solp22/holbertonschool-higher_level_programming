ad55ca6f71eac54321773a50308e47f23f91e1b3
solp22 <sol.puentereal@gmail.com>
2023-06-20 12:37:50 -0700

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -23,5 +23,5 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
-        self.integer_validator("size", size)
+        self.__size = self.integer_validator("size", size)
         super().__init__(size, size)
