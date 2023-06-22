a9c754b866f8f1220f157d916093a3084f363a73
solp22 <sol.puentereal@gmail.com>
2023-06-22 16:05:37 -0700

    pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     objs = load_from_json_file("add_item.json")
-except  FileNotFoundError:
+except FileNotFoundError:
     objs = []
 save_to_json_file(objs + argv[1:], "add_item.json")
