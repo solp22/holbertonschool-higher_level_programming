7a8f44d365337b8c0a796e9dd58c2d451fe9116c
solp22 <sol.puentereal@gmail.com>
2023-06-23 09:53:34 -0700

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(self, Student):
+            if attrs is None:
+                return self.__dict__
+        dict = {}
+        for element in attrs:
+            if element in self.__dict__.keys():
+                dict[element] = self.__dict__[element]
+        return dict
+    def reload_from_json(self, json):
+        self.first_name = json.get("first_name")
+        self.last_name = json.get("last_name")
+        self.age = json.get("age")
