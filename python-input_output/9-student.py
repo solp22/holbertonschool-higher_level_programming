aa04c14f6d9fedeeea3647f4d7a0f90be6831792
solp22 <sol.puentereal@gmail.com>
2023-06-22 20:17:07 -0700

    pycodestyle

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,7 +6,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         if isinstance(self, Student):
             return self.__dict__
