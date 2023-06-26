ef192d857aae5da01931422b37a44777cc189343
solp22 <sol.puentereal@gmail.com>
2023-06-26 10:19:09 -0700

    task 1 fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,11 +5,10 @@
 class Base:
     """initialize class"""
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
-        
         if id is not None:
             self.id = id
         else:
-            __nb_objects = __nb_objects + 1
-            self.id = __nb_objects
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
