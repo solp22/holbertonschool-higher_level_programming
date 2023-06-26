c65980a8d8661d94712ad811311b6a539749c314
solp22 <sol.puentereal@gmail.com>
2023-06-26 11:53:11 -0700

    documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define class"""
 
+
 from models.base import Base
 
 class Rectangle(Base):
@@ -11,35 +12,35 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         self.__height = value
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         self.__width = value
-        
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         self.__x = value
-    
+
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         self.__y = value
