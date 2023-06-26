fd8f8fc5bf0e5eeb33ea6fb1e72486672b97fe02
solp22 <sol.puentereal@gmail.com>
2023-06-26 06:41:58 -0700

    task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define the pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal's triangle of n"""
+    triangle = [[1]]
+    for tri in range(n - 1):
+        new = []
+        for lst in range(len(triangle[tri]) + 1):
+            if lst == 0 or lst == len(triangle[tri]):
+                new.append(1)
+            else:
+                new.append(triangle[tri][lst - 1] + triangle[tri][lst])
+        triangle.append(new)
+    return triangle
