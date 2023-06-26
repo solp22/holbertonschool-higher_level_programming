a9080d7d8a908373879408aa554386b83f31521e
solp22 <sol.puentereal@gmail.com>
2023-06-26 10:05:19 -0700

    task 1

---
+++ python-almost_a_circle/models/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Base:
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects = __nb_objects + 1
+            self.id = __nb_objects
