3f3b0b11a8d13c982f8a742b98ec7b7aa7d609d1
solp22 <sol.puentereal@gmail.com>
2023-06-27 16:36:47 -0700

    decorator

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """json representation of list_dictionaries"""
         if list_dictionaries is None:
