0f9caa3498360ef666986c03d4177036bd39b3c3
solp22 <sol.puentereal@gmail.com>
2023-06-27 15:22:37 -0700

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize class"""
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """prints square data"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
