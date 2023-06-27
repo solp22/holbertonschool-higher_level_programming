e2eeb6aa833150376a5e85ac0cea8da1234f70c6
solp22 <sol.puentereal@gmail.com>
2023-06-27 11:05:27 -0700

    fix task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,14 +79,13 @@ class Rectangle(Base):
 
     def update(self, *args):
         """assign argument to each attribute"""
-        for i, arg in enumerate(args):
-            if i == 0:
-                Base.id = arg
-            if i == 1:
-                self.width = arg
-            if i == 2:
-                self.height = arg
-            if i == 3:
-                self.x = arg
-            if i == 4:
-                self.y = arg
+        if len(args) > 4:
+            self.__y = args[4]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 0:
+            self.id = args[0]
