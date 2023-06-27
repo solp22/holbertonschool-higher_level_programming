426a6342e37e6eef76d12ad5e86538bf0631373f
solp22 <sol.puentereal@gmail.com>
2023-06-27 15:22:57 -0700

    pycode

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initialize class"""
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """prints square data"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
