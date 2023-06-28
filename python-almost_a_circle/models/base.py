9acd2494b6f55c16bb0b29cab020281207059474
solp22 <sol.puentereal@gmail.com>
2023-06-27 18:21:33 -0700

    task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,3 +21,19 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save json representation in a file"""
+        if list_objs is None:
+            list_objs = []
+        for i in list_objs:
+            with open(f"{cls.__name__}.json", 'w', encoding="utf-8") as f:
+                f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """convert to python"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
