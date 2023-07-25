2d53785361a41e293202b2301f0938c8a295e9dd
solp22 <sol.puentereal@gmail.com>
2023-07-25 08:14:22 -0700

    fix

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- create table w foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INT NOT NULL AUTO_INCREMENT,
-  state_id INT NOT NULL,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  state_id INTEGER NOT NULL,
   name VARCHAR(256) NOT NULL,
-  PRIMARY KEY (id),
   FOREIGN KEY (state_id) REFERENCES states(id)
 );
