37ccbbdb13bf3b0574258eb7a8b05d37c1ecfa28
solp22 <sol.puentereal@gmail.com>
2023-07-25 06:38:35 -0700

    ?????????

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,7 @@
 -- create user
+
 FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
 FLUSH PRIVILEGES;
