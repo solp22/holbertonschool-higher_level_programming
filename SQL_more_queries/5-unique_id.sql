c92b0bdc9143747f38c7e11545ea28cd4512787d
solp22 <sol.puentereal@gmail.com>
2023-07-25 07:16:08 -0700

    fix

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- create table w default and unique id
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
