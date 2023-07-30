ca456f97b2df4eef6d5bab463b997472e6cfa16b
solp22 <sol.puentereal@gmail.com>
2023-07-30 15:48:59 -0700

    first state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all state objects"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = session.query(State).order_by(State.id).first()
+    if not instance:
+        print("Nothing")
+    print(f"{instance.id}: {instance.name}")
