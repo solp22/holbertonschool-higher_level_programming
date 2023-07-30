1a477de6a323c0be6d665710558b90ce72a09650
solp22 <sol.puentereal@gmail.com>
2023-07-30 13:06:55 -0700

    sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""takes an arg and displays all values in table where name matches, safe from injections"""
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC",
+        (state_name_searched,))
+    rows = cur.fetchall()
+    for result in rows:
+        print(result)
+    cur.close()
+    db.close()
