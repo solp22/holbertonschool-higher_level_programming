cbabc2220534cb53cb83da1c64a225537624061b
solp22 <sol.puentereal@gmail.com>
2023-07-30 14:13:33 -0700

    pycode

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
         port=3306
     )
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities 
-        INNER JOIN states ON cities.state_id = states.id 
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+        INNER JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for result in rows:
