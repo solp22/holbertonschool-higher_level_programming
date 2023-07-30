b13d22e17a9f1a5c6f56aa8efb4c9ff986360e8d
solp22 <sol.puentereal@gmail.com>
2023-07-30 11:49:14 -0700

    get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for result in rows:
+        print(result)
+    cur.close()
+    db.close()
