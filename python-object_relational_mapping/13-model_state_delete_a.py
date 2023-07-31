dec7f67aadf90d274b733329d3c10a7d4e752ada
solp22 <sol.puentereal@gmail.com>
2023-07-30 17:06:09 -0700

    pycode

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     session.query(State).filter(State.name.like('%a%')).delete()
     session.commit()
